cc242b72dbfde7cdca51d1954e90b11cd1c3da22
David Vinegar <dbvinegar@email.wm.edu>
2016-12-23 14:23:25 -0500

    Initial commit

---
+++ app/src/main/java/com/davidvinegar/tacoria/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.davidvinegar.tacoria;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by davidvinegar on 12/23/16.
+ */
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
