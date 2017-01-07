a7c496551059985e6ca2382092e4f656e4d01179
David Vinegar <dvinegar3@gatech.edu>
2017-01-07 12:50:12 -0500

    added in creditcard activity and it works.  would like to split out cvv and expiration

--- app/src/main/java/com/davidvinegar/tacoria/Activities/CreditCardInputActivity.java
+++ app/src/main/java/com/davidvinegar/tacoria/Activities/CreditCardInputActivity.java
@@ -2,12 +2,11 @@ package com.davidvinegar.tacoria.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.davidvinegar.tacoria.R;
+import com.devmarvel.creditcardentry.library.CardValidCallback;
 import com.devmarvel.creditcardentry.library.CreditCard;
 import com.devmarvel.creditcardentry.library.CreditCardForm;
 
@@ -15,32 +14,43 @@ import com.devmarvel.creditcardentry.library.CreditCardForm;
  * Created by davidvinegar on 1/6/17.
  */
 public class CreditCardInputActivity extends Activity{
-    private LinearLayout linearLayout;
-    private CreditCardForm form;
     private Button buttonAuthorize;
+
+    CardValidCallback cardValidCallback = new CardValidCallback() {
+        @Override
+        public void cardValid(CreditCard creditCard) {
+            Toast.makeText(CreditCardInputActivity.this,"Card Valid and Complete", Toast.LENGTH_LONG);
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.getcreditcard_layout);
-        linearLayout = (LinearLayout)findViewById(R.id.credit_linear_layer);
 
-        form = new CreditCardForm(this);
-        linearLayout.addView(form);
+        final CreditCardForm form = (CreditCardForm) findViewById(R.id.credit_card_form);
+        form.setOnCardValidCallback(cardValidCallback);
 
-        buttonAuthorize = (Button) findViewById(R.id.button_authorize_cc);
-        buttonAuthorize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(form.isCreditCardValid()){
-                    CreditCard card = form.getCreditCard();
 
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Invalid Credit Card",Toast.LENGTH_LONG);
-                }
-            }
-        });
+
+//
+//        buttonAuthorize = (Button) findViewById(R.id.button_authorize_cc);
+//        buttonAuthorize.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(form.isCreditCardValid()){
+//                    CreditCard card = form.getCreditCard();
+//
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(),"Invalid Credit Card",Toast.LENGTH_LONG);
+//                }
+//            }
+//        });
 
     }
+
+
+
 
 }
