62a646301492fcbe987065ae3ef2b3b79c74e5a4
David Vinegar <dvinegar3@gatech.edu>
2017-01-09 22:06:30 -0500

    view menu now links to website page

--- app/src/main/java/com/davidvinegar/tacoria/Activities/MainActivity.java
+++ app/src/main/java/com/davidvinegar/tacoria/Activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.davidvinegar.tacoria.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.davidvinegar.tacoria.R;
 
@@ -14,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.viewMenu);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://eattacoria.com/tacoria-menu-2016.pdf"));
+                startActivity(browserIntent);
+            }
+        });
 
     }
 
